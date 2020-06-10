d68ae62011f6b08643856cfa1c00836b4264ded5
Adam Blackwater <me@adamblackwater.com>
2020-06-10 15:55:18 +0100

    Fix

--- Makefile
+++ Makefile
@@ -1,10 +1,3 @@
-export DB_HOST?=127.0.0.1
-export DB_PASS?=admin123
-export DB_USER?=admin123
-export DB_NAME?=risk
-export DB_PORT?=3306
-
-
 up:
 	docker-compose up -d
 
--- app.py
+++ app.py
@@ -1,25 +1,7 @@
-import os
-import aiomysql
 from aiohttp import web
 import combat
 
-
-async def create_db_pool(app):
-    app['pool'] = await aiomysql.create_pool(
-            host=os.environ['DB_HOST'],
-            user=os.environ['DB_USER'],
-            password=os.environ['DB_PASS'],
-            db=os.environ['DB_NAME'],
-            port=int(os.environ['DB_PORT'])
-        )
-
-
-async def close_db_conn(app):
-    await app['pool'].close()
-
 app = web.Application()
-
-app.on_startup.append(create_db_pool)
 
 app.add_routes([
     web.post('/v0/basic-combat', combat.basic_combat)
